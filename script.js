d8a5c7c83c2aee28289c86a52158cd1269d4738c
vinay choudhary <vinaychoudhary7668@gmail.com>
2024-05-19 20:30:52 +0530

    my codsoft intrnship task 3

---
+++ script.js
@@ -0,0 +1,31 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendCharacter(character) {
+    let display = document.getElementById('display');
+    display.value += character;
+}
+
+function calculateResult() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
+
+function calculateSquareRoot() {
+    let display = document.getElementById('display');
+    try {
+        display.value = Math.sqrt(eval(display.value));
+    } catch {
+        display.value = 'Error';
+    }
+}
